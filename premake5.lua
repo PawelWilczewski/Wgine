a7e546f2d786c915a8292fbbc95af94096648aec
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-08-24 16:51:32 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -16,119 +16,121 @@ IncludeDir["GLFW"] = "Wgine/thirdparty/GLFW/include"
 IncludeDir["Glad"] = "Wgine/thirdparty/Glad/include"
 IncludeDir["ImGui"] = "Wgine/thirdparty/ImGui"
 
-include "Wgine/thirdparty/GLFW"
-include "Wgine/thirdparty/Glad"
-include "Wgine/thirdparty/ImGui"
+group "Dependencies"
+	include "Wgine/thirdparty/GLFW"
+	include "Wgine/thirdparty/Glad"
+	include "Wgine/thirdparty/ImGui"
 
-project "Wgine"
-	location "Wgine"
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "off"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "WginePCH.h"
-	pchsource "Wgine/src/WginePCH.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/thirdparty/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines {
-			"WGINE_PLATFORM_WINDOWS",
-			"WGINE_BUILD_DLL",
-			"GLFW_INCLUDE_NONE",
-			"IMGUI_IMPL_OPENGL_LOADER_CUSTOM"
-		}
-
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-		}
-
-	filter "configurations:Debug"
-		defines "WGINE_DEBUG"
-		runtime "Debug"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "WGINE_RELEASE"
-		runtime "Release"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "WGINE_DIST"
-		runtime "Release"
-		optimize "On"
-
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-	}
-
-	includedirs
-	{
-		"Wgine/thirdparty/spdlog/include",
-		"Wgine/src"
-	}
-
-	links
-	{
-		"Wgine"
-	}
-
-	filter "system:windows"
+group ""
+	project "Wgine"
+		location "Wgine"
+		kind "SharedLib"
+		language "C++"
 		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
+		staticruntime "off"
 
-		defines {
-			"WGINE_PLATFORM_WINDOWS"
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+		pchheader "WginePCH.h"
+		pchsource "Wgine/src/WginePCH.cpp"
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
 		}
 
-	filter "configurations:Debug"
-		defines "WGINE_DEBUG"
-		symbols "On"
+		includedirs
+		{
+			"%{prj.name}/src",
+			"%{prj.name}/thirdparty/spdlog/include",
+			"%{IncludeDir.GLFW}",
+			"%{IncludeDir.Glad}",
+			"%{IncludeDir.ImGui}"
+		}
 
-	filter "configurations:Release"
-		defines "WGINE_RELEASE"
-		optimize "On"
+		links
+		{
+			"GLFW",
+			"Glad",
+			"ImGui",
+			"opengl32.lib"
+		}
 
-	filter "configurations:Dist"
-		defines "WGINE_DIST"
-		optimize "On"
+		filter "system:windows"
+			systemversion "latest"
+
+			defines {
+				"WGINE_PLATFORM_WINDOWS",
+				"WGINE_BUILD_DLL",
+				"GLFW_INCLUDE_NONE",
+				"IMGUI_IMPL_OPENGL_LOADER_CUSTOM"
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			}
+
+		filter "configurations:Debug"
+			defines "WGINE_DEBUG"
+			runtime "Debug"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "WGINE_RELEASE"
+			runtime "Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "WGINE_DIST"
+			runtime "Release"
+			optimize "On"
+
+
+	project "Sandbox"
+		location "Sandbox"
+		kind "ConsoleApp"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp",
+		}
+
+		includedirs
+		{
+			"Wgine/thirdparty/spdlog/include",
+			"Wgine/src"
+		}
+
+		links
+		{
+			"Wgine"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "latest"
+
+			defines {
+				"WGINE_PLATFORM_WINDOWS"
+			}
+
+		filter "configurations:Debug"
+			defines "WGINE_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "WGINE_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "WGINE_DIST"
+			optimize "On"
