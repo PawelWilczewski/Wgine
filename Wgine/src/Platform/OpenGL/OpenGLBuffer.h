bb68ff02467ccee6c214a7a73093adf13d9de6f3
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-08-25 22:39:40 +0100

    Buffer layouts (i. e. vertex colors made easy)

--- Wgine/src/Platform/OpenGL/OpenGLBuffer.h
+++ Wgine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,11 +10,15 @@ namespace Wgine
 		OpenGLVertexBuffer(float *vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		inline virtual void SetLayout(const BufferLayout &layout) override { m_Layout = layout; }
+		inline virtual const BufferLayout &GetBufferLayout() const override { return m_Layout; }
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -23,8 +27,8 @@ namespace Wgine
 		OpenGLIndexBuffer(uint32_t *indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 
