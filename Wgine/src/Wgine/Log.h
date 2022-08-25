83433e3b47a78a2bb59bab85d337dc438120d529
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-08-25 22:48:44 +0100

    Update Log.h

--- Wgine/src/Wgine/Log.h
+++ Wgine/src/Wgine/Log.h
@@ -24,11 +24,11 @@ namespace Wgine {
 #define WGINE_CORE_INFO(...)  ::Wgine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define WGINE_CORE_WARN(...)  ::Wgine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define WGINE_CORE_ERROR(...) ::Wgine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define WGINE_CORE_FATAL(...) ::Wgine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define WGINE_CORE_FATAL(...) ::Wgine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client macros
 #define WGINE_TRACE(...) ::Wgine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define WGINE_INFO(...)  ::Wgine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define WGINE_WARN(...)  ::Wgine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define WGINE_ERROR(...) ::Wgine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define WGINE_FATAL(...) ::Wgine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define WGINE_FATAL(...) ::Wgine::Log::GetClientLogger()->critical(__VA_ARGS__)
