c75dab7b02272165e806181ac43944eca3cc8047
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-09-04 05:23:00 +0100

    MakeRef, MakeScope constexpr added to replace make_shared and make_unique

--- Wgine/src/Wgine/Core/Core.h
+++ Wgine/src/Wgine/Core/Core.h
@@ -45,7 +45,11 @@ namespace Wgine
 {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ...Args>
+	constexpr Ref<T> MakeScpe(Args &&...args) { return std::make_unique<T>(std::forward<Args>(args)...); }
 
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+	template<typename T, typename ...Args>
+	constexpr Ref<T> MakeRef(Args &&...args) { return std::make_shared<T>(std::forward<Args>(args)...); }
 }
