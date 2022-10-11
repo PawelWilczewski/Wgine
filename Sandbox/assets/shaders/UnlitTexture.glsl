72157e7cbdac27191ad40656aace11b01f48fc26
Paweł Wilczewski <wilczewski-pawel@wp.pl>
2022-10-11 15:53:01 +0100

    Update UnlitTexture.glsl

--- Sandbox/assets/shaders/UnlitTexture.glsl
+++ Sandbox/assets/shaders/UnlitTexture.glsl
@@ -1,11 +1,10 @@
 #type vertex
-#version 330 core
+#version 460 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_TexCoord;
 layout(location = 3) in vec3 a_Normal;
-//layout(location = 4) in mat4 a_Transform;
 
 uniform mat4 u_ViewProjection;
 uniform int u_MaterialID[512];
@@ -13,7 +12,7 @@ uniform int u_MaterialID[512];
 out vec4 v_Color;
 out vec2 v_TexCoord;
 out vec3 v_Normal;
-out float v_MaterialID;
+out flat int v_MaterialID;
 
 void main()
 {
@@ -27,7 +26,14 @@ void main()
 }
 
 #type fragment
-#version 330 core
+#version 460 core
+
+layout (std430, binding=0) buffer s_Materials
+{ 
+  vec4 camera_position;
+  vec4 light_position;
+  vec4 light_diffuse;
+};
 
 struct Material
 {
@@ -43,7 +49,7 @@ layout(location = 0) out vec4 Color;
 in vec2 v_TexCoord;
 in vec4 v_Color;
 in vec3 v_Normal;
-in float v_MaterialID;
+in flat int v_MaterialID;
 
 uniform sampler2D u_Texture[32];
 uniform vec2 u_Tiling; // TODO: tiling implemented per-texture (how?) (in material per texture?)
