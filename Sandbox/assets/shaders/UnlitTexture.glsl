2fd692d9b91a9aa846af7694c23d7a4c8e055ca9
Paweł Wilczewski <wilczewski-pawel@wp.pl>
2022-10-11 10:40:09 +0100

    Update UnlitTexture.glsl

--- Sandbox/assets/shaders/UnlitTexture.glsl
+++ Sandbox/assets/shaders/UnlitTexture.glsl
@@ -1,15 +1,6 @@
 #type vertex
 #version 330 core
 
-struct Material
-{
-	vec3 Diffuse;
-	vec3 Specular;
-	vec3 Ambient;
-	int DiffuseTex;
-	int SpecularTex;
-};
-
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_TexCoord;
@@ -17,7 +8,6 @@ layout(location = 3) in vec3 a_Normal;
 //layout(location = 4) in mat4 a_Transform;
 
 uniform mat4 u_ViewProjection;
-uniform Material u_Material;
 
 out vec4 v_Color;
 out vec2 v_TexCoord;
@@ -36,6 +26,15 @@ void main()
 #type fragment
 #version 330 core
 
+struct Material
+{
+	vec3 Diffuse;
+	vec3 Specular;
+	vec3 Ambient;
+	int DiffuseTex;
+	int SpecularTex;
+};
+
 layout(location = 0) out vec4 Color;
 
 in vec2 v_TexCoord;
@@ -43,7 +42,8 @@ in vec4 v_Color;
 in vec3 v_Normal;
 
 uniform sampler2D u_Texture[32];
-uniform vec2 u_Tiling;
+uniform vec2 u_Tiling; // TODO: tiling implemented per-texture (how?) (int material?)
+uniform Material u_Material;
 
 void main()
 {
