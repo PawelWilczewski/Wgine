7f873e80153753c3fe3ed8ec0b33b0e882d62612
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-10-20 18:06:13 +0100

    Added flat shading normals calculation

--- Wgine/src/Wgine/Renderer/Mesh.h
+++ Wgine/src/Wgine/Renderer/Mesh.h
@@ -17,7 +17,7 @@ namespace Wgine
 		Mesh(Vertex *triVertices, uint32_t verticesCount, uint32_t *indices, uint32_t indicesCount)
 			: m_Vertices(triVertices, triVertices + verticesCount), m_Indices(indices, indices + indicesCount)
 		{
-			RecalculateNormals();
+			RecalculateNormalsFlat();
 		}
 
 		const std::vector<Vertex> &GetVertices() const { return m_Vertices; }
@@ -70,60 +70,76 @@ namespace Wgine
 			// for each face a
 			for (int i = 0; i < m_Indices.size(); i += 3)
 			{
-				Vertex av0 = m_Vertices[m_Indices[i]];
-				Vertex av1 = m_Vertices[m_Indices[i + 1]];
-				Vertex av2 = m_Vertices[m_Indices[i + 2]];
+				Vertex &av0 = m_Vertices[m_Indices[i]];
+				Vertex &av1 = m_Vertices[m_Indices[i + 1]];
+				Vertex &av2 = m_Vertices[m_Indices[i + 2]];
 
 				auto aNormal = glm::normalize(glm::cross(
 					av0.Position - av1.Position,
 					av2.Position - av1.Position
 				));
 
-				// for each vertex in a
-				for (int vert = 0; vert < 3; vert++)
+				// for each face b
+				for (int j = 0; j < m_Indices.size(); j += 3)
 				{
-					// for each face b
-					for (int j = 0; j < m_Indices.size(); j += 3)
+					// skip if self
+					if (j == i) continue;
+
+					Vertex &bv0 = m_Vertices[m_Indices[j]];
+					Vertex &bv1 = m_Vertices[m_Indices[j + 1]];
+					Vertex &bv2 = m_Vertices[m_Indices[j + 2]];
+
+					// b normal
+					auto bCross = glm::cross(
+						bv0.Position - bv1.Position,
+						bv2.Position - bv1.Position
+					);
+					auto bNormal = glm::normalize(bCross);
+					auto bArea = bCross.length() * 0.5f;
+
+					// a and b share av0
+					if (m_Indices[i] == m_Indices[j] || m_Indices[i] == m_Indices[j + 1] || m_Indices[i] == m_Indices[j + 1])
 					{
-						// skip if self
-						if (j == i) continue;
-
-						Vertex bv0 = m_Vertices[m_Indices[j]];
-						Vertex bv1 = m_Vertices[m_Indices[j + 1]];
-						Vertex bv2 = m_Vertices[m_Indices[j + 2]];
-
-						// b normal
-						auto bCross = glm::cross(
-							bv0.Position - bv1.Position,
-							bv2.Position - bv1.Position
-						);
-						auto bNormal = glm::normalize(bCross);
-						auto bArea = bCross.length() * 0.5f;
-
-						// a and b share av0
-						if (m_Indices[i] == m_Indices[j] || m_Indices[i] == m_Indices[j + 1] || m_Indices[i] == m_Indices[j + 1])
-						{
-							auto angle = angleBetweenVectors(av0.Position - av1.Position, av0.Position - av2.Position);
-							aNormal += bNormal * bArea * angle;
-						}
-
-						// a and b share av1
-						if (m_Indices[i + 1] == m_Indices[j] || m_Indices[i + 1] == m_Indices[j + 1] || m_Indices[i + 1] == m_Indices[j + 1])
-						{
-							auto angle = angleBetweenVectors(av1.Position - av0.Position, av1.Position - av2.Position);
-							aNormal += bNormal * bArea * angle;
-						}
-
-						// a and b shader av2
-						if (m_Indices[i + 2] == m_Indices[j] || m_Indices[i + 2] == m_Indices[j + 1] || m_Indices[i + 2] == m_Indices[j + 1])
-						{
-							auto angle = angleBetweenVectors(av2.Position - av0.Position, av2.Position - av1.Position);
-							aNormal += bNormal * bArea * angle;
-						}
+						auto angle = angleBetweenVectors(av0.Position - av1.Position, av0.Position - av2.Position);
+						//aNormal += bNormal * bArea * angle;
+						av0.Normal += aNormal + bNormal * bArea * angle;
+						//av0.Normal += aNormal;
 					}
 
-					m_Vertices[m_Indices[i + vert]].Normal = glm::normalize(aNormal);
+					// a and b share av1
+					if (m_Indices[i + 1] == m_Indices[j] || m_Indices[i + 1] == m_Indices[j + 1] || m_Indices[i + 1] == m_Indices[j + 1])
+					{
+						auto angle = angleBetweenVectors(av1.Position - av0.Position, av1.Position - av2.Position);
+						//aNormal += bNormal * bArea * angle;
+						av1.Normal += aNormal + bNormal * bArea * angle;
+						//av1.Normal += aNormal;
+					}
+
+					// a and b share av2
+					if (m_Indices[i + 2] == m_Indices[j] || m_Indices[i + 2] == m_Indices[j + 1] || m_Indices[i + 2] == m_Indices[j + 1])
+					{
+						auto angle = angleBetweenVectors(av2.Position - av0.Position, av2.Position - av1.Position);
+						//aNormal += bNormal * bArea * angle;
+						av2.Normal += aNormal + bNormal * bArea * angle;
+						//av2.Normal += aNormal;
+					}
 				}
+			}
+
+			for (int i = 0; i < m_Indices.size(); i++)
+				m_Vertices[m_Indices[i]].Normal = glm::normalize(m_Vertices[m_Indices[i]].Normal);
+		}
+
+		// must not share vertices
+		void RecalculateNormalsFlat()
+		{
+			// for each face a
+			for (int i = 0; i < m_Indices.size(); i += 3)
+			{
+				auto n = glm::normalize(glm::cross(m_Vertices[m_Indices[i]].Position - m_Vertices[m_Indices[i + 2]].Position, m_Vertices[m_Indices[i]].Position - m_Vertices[m_Indices[i + 1]].Position));
+				m_Vertices[m_Indices[i]].Normal = n;
+				m_Vertices[m_Indices[i + 1]].Normal = n;
+				m_Vertices[m_Indices[i + 2]].Normal = n;
 			}
 		}
 
