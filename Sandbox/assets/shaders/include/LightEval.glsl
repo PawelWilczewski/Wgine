c67a995385237577666929ec003fcce6491e4e20
PawelWilczewski <pawel.wilczewski@outlook.com>
2022-11-18 04:52:52 +0000

    Directional light implementation

--- Sandbox/assets/shaders/include/LightEval.glsl
+++ Sandbox/assets/shaders/include/LightEval.glsl
@@ -5,7 +5,17 @@ vec3 EvaluateAmbientLight(AmbientLight light, Material mat, vec3 matDiffuse, flo
 
 vec3 EvaluateDirectionalLight(DirectionalLight light, Material mat, vec3 matDiffuse, float matSpecular, vec3 normal)
 {
-	return vec3(0.0);
+	// diffuse lighting
+	float diff = max(dot(normal, light.Direction), 0.0);
+	vec3 diffuse = matDiffuse * diff * light.Color;
+
+	// specular lighting
+	vec3 viewDir = normalize(u_CameraLocation - io_WorldPos);
+	vec3 reflectDir = reflect(-light.Direction, normal); 
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), mat.Shininess);
+	vec3 specular = matSpecular * spec * light.Color;
+
+	return (diffuse + specular) * light.Intensity;
 }
 
 vec3 EvaluatePointLight(PointLight light, Material mat, vec3 matDiffuse, float matSpecular, vec3 normal)
@@ -51,6 +61,7 @@ vec3 EvaluateSpotLight(SpotLight light, Material mat, vec3 matDiffuse, float mat
     float d = max(length(light.Location - io_WorldPos) - light.Radius, 0.0) / light.CutoffRadius; // normalized distance respecting the light radius
 	float attenuation = light.Intensity / (d * d); // inverse square, div by 0 possible but the perf hit of removing that is not worth it
 
+	// spot light mask
 	float angle = degrees(dot(normalize(light.Location - io_WorldPos), light.Direction));
 	attenuation *= clamp((angle - light.CutoffAngleInner) / (light.CutoffAngle - light.CutoffAngleInner), 0.0, 1.0);
 
